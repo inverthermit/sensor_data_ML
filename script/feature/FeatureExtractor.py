1f7c431fc837df976f049455d21c171c29bf76f7
Tim <l1111j@live.com>
2017-12-19 14:35:50 +1100

    Update: add back

---
+++ script/feature/FeatureExtractor.py
@@ -0,0 +1,10 @@
+
+
+class FeatureExtractor:
+    "Generates agents for a side"
+
+    def __init__(self):
+      """nothing"""
+
+    def getFeature(self):
+      print('Error: This is the parent class.')
